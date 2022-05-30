b41f6a277c8b51460b28c94c6e1a429737437fbc
Janek Paessens <paessens@ids.rwth-aachen.de>
2022-05-30 10:59:02 +0200

    Adding possibility to run simulations with different N

--- run_simulations.py
+++ run_simulations.py
@@ -1,15 +1,13 @@
 import subprocess
 
 additional_options = {
-    "miss_spikes_10&&ms_10" : {"-p_dont_send_spike" : "0.1"},
-    "miss_spikes_10&&ms_20" : {"-p_dont_send_spike" : "0.2"},
-    "miss_spikes_10&&ms_30" : {"-p_dont_send_spike" : "0.3"},
-    "miss_spikes_10&&ms_40" : {"-p_dont_send_spike" : "0.4"},
+
 }
 
+N = 400
 num_executions = 1
 start = 1
-test_mode = True
+test_mode = False
 fashion_mnist = False
 
 def dict_to_args(dict):
@@ -29,12 +27,12 @@ for label in additional_options:
     for i in range(start, start+num_executions):
         label_with_index = '%s_%d' % (label, i)
         nohup_file = '/mnt/data4tb/paessens/simulations/%s/simulation_%s.out' % (label_with_index, 'test_'+test_label if test_mode else 'train')
-        cmd_init = 'python init_directory_structure.py -label %s' % label_with_index
-        cmd = 'python -u simulate.py -label %s -mode %s %s' % (label_with_index, 'test' if test_mode else 'train', args)
+        cmd_init = 'python init_directory_structure.py -label %s -N %d' % (label_with_index, N)
+        cmd = 'python -u simulate.py -label %s -mode %s %s -N %d' % (label_with_index, 'test' if test_mode else 'train', args, N)
         if fashion_mnist:
             cmd += ' -data fashion_mnist/'
-        if test_label is not None:
-            cmd += "-test_label "+test_label
+        if test_mode:
+            cmd += " -test_label "+test_label
         
         if not test_mode:
             print('Running '+cmd_init)
